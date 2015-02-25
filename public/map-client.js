15110524b972a61d36c29513b37c19f490eb135f
tereith <terje.eithun@gmail.com>
2015-02-25 15:38:59 +0100

    added import gpx btn (no func)

--- public/map-client.js
+++ public/map-client.js
@@ -1,46 +1,50 @@
-var map;
-console.log("map-client.js loading...")
+(function () {
+    "use strict"
 
-function init() {
-    console.log("init....")
-    map = new ol.Map({
-        target: 'map',
-        renderer: 'canvas',
-        view: new ol.View({
-            projection: 'EPSG:900913',
-            center: [1157172.95294, 9206597.85639],
-            zoom: 12
-        })
-    });
-    var openStreetMapLayer = new ol.layer.Tile({
-        source: new ol.source.OSM()
-    });
+    var map;
 
-    map.on("singleclick", function (evt) {
-        //console.log(evt);
-        //console.log(evt.coordinate);
-        writeCoordinatesToHtml(evt.coordinate);
-    });
-    
-/*    map.on("singleclick", function (evt) {
-        //console.log(evt);
-        //console.log(evt.coordinate);
-        writeCoordinatesToHtml(evt.coordinate);
-    });*/
+    (function init() {
+        console.log("init....")
+        map = new ol.Map({
+            target: 'map',
+            renderer: 'canvas',
+            view: new ol.View({
+                projection: 'EPSG:900913',
+                center: [1157172.95294, 9206597.85639],
+                zoom: 12
+            })
+        });
 
-    map.addLayer(openStreetMapLayer);
-}
-init();
+        var openStreetMapLayer = new ol.layer.Tile({
+            source: new ol.source.OSM()
+        });
+        map.addLayer(openStreetMapLayer);
 
-function writeCoordinatesToHtml(coordinates) {
-    var x;
-    var y;
-    x = coordinates[0];
-    y = coordinates[1];
-    console.log("x = " + x);
-    console.log("y = " + y);
-    /*$(".coordinates").css("background-color", "red");*/
-    $(".coordinates").empty();
-    $(".coordinates").append("<p>x = " + x + "</br>y = " + y + "</p>");
+        map.on("click", function (evt) {
+            console.log(evt);
+            //console.log(evt.coordinate);
+            writeCoordinatesToHtml(evt.coordinate);
+        });
 
-};
+        $("#import-gpx-btn").click(function () {
+            confirm("Import GPX action here..");
+/*            this.file().choose(function(e, input) {
+                alert(input.val()); //alerts the chosen filename.
+            });*/
+        });
+
+    })();
+
+    function writeCoordinatesToHtml(coordinates) {
+        var x;
+        var y;
+        x = coordinates[0];
+        y = coordinates[1];
+        console.log("x = " + x);
+        console.log("y = " + y);
+        /*$(".coordinates").css("background-color", "red");*/
+        $(".coordinates").empty();
+        $(".coordinates").append("<p>x = " + x + "</br>y = " + y + "</p>");
+
+    };
+})();
